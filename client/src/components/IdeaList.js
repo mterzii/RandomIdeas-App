83898422d5e8b448facafdf9aedd4994e99d07c9
Mert Terzi <mterzi.143@hotmail.com>
2025-09-30 00:10:42 +0300

    last update

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -1,23 +1,12 @@
+import IdeasApi from "../services/ideasApi";
+
 class IdeaList {
   constructor() {
     this._ideaListEl = document.querySelector('#idea-list');
 
-    this._ideas = [
-      {
-        id: 1,
-        text: 'Idea 1',
-        tag: 'Business',
-        username: 'John',
-        date: '02/01/2023',
-      },
-      {
-        id: 2,
-        text: 'Idea 2',
-        tag: 'Technology',
-        username: 'Jill',
-        date: '02/01/2023',
-      },
-    ];
+    this._ideas = [];
+    this.getTagClass();
+
     this._validTags = new Set();
     this._validTags.add('technology');
     this._validTags.add('software');
@@ -25,18 +14,47 @@ class IdeaList {
     this._validTags.add('education');
     this._validTags.add('health');
     this._validTags.add('inventions');
-
-
   }
+
+  async getIdeas() 
+  {
+    try 
+    {
+      const res = await IdeasApi.getIdeas();
+      this._ideas = res.data.data;
+      console.log(this._ideas);
+    } 
+    catch (error) 
+    {
+      console.log(error);
+    }
+}
+
+
+  getTagClass(tag) 
+  {
+    tag = tag.toLowerCase();   // tag'i küçük harfe çeviriyor
+    let tagClass = '';
+
+    if (this._validTags.has(tag)) {
+      tagClass = `tag-${tag}`;  // eğer tag geçerliyse class ismi oluştur
+    } else {
+      tagClass = '';            // geçerli değilse boş bırak
+    }
+
+    return tagClass;            // sonucu döndür
+}
+
 
   render() {
   this._ideaListEl.innerHTML = this._ideas
     .map((idea) => {
+      const tagClass=this.getTagClass(idea.tag);
       return `
         <div class="card">
           <button class="delete"><i class="fas fa-times"></i></button>
           <h3>${idea.tag.toUpperCase()}</h3>
-          <p class="tag tag-technology">${idea.tag.toUpperCase()}</p>
+          <p class="tag ${tagClass}">${idea.tag.toUpperCase()}</p>
           <p>
             Posted on <span class="date">${idea.date}</span> by
             <span class="author">${idea.username}</span>
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path=require('path'); //Dosya ve klasör yollarını (path) işletim sistemine uygun Node.js Modülü
 const express = require('express');   // 1. express paketini projeye dahil et
+const cors=require('cors');
 require('dotenv').config();//Normalde Node.js .env dosyasını tanımaz.dotenv paketi bunu çözer.config ayarları yükle der
 const port =process.env.PORT || 5000;  // 2. hangi portta çalışacağını belirle http://localhost:5000
 //.env dosyasında PORT=5000 varsa, kodda process.env.PORT yazınca sana "3000"
@@ -23,6 +24,17 @@ app.use(express.static(path.join(__dirname, 'public')));//Her istekte şunu çal
 app.use(express.json());//gelen isteğin (body) JSON formatındaysa, otomatik cevir req.body koy
 app.use(express.urlencoded({ extended: false }));//gelen veri eğer HTML formundan gönderildiyse onu da çözümleyip req.body koyar
 //extended: false → sadece basit key=value formatlarını
+
+//cors middleware
+app.use
+(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
+
+
 
 app.get('/',(req,res)=> //http://localhost:5000 yazdığında (veya sonuna / eklediğinde), bu rota tetiklenecektir.
 {
